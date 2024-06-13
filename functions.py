249e5b77f0f9f18acf56b42c4ef2b832f57784bf
Paulaszka <pkropkasz@gmail.com>
2024-06-13 16:01:58 +0200

    everything work on telescope data

--- functions.py
+++ functions.py
@@ -5,6 +5,9 @@ from sklearn.preprocessing import StandardScaler
 from imblearn.over_sampling import RandomOverSampler
 from sklearn.metrics import classification_report
 from sklearn.svm import SVC
+import tensorflow as tf
+from keras.src.utils import to_categorical
+from sklearn.metrics import *
 
 
 def prepare_data(file_path):
@@ -32,5 +35,94 @@ def oversample_set(data_set, oversample=False):
     connected = np.hstack((x, np.reshape(y, (-1, 1))))
     return connected, x, y
 
+def plot_loss(history):
+    plt.plot(history.history['loss'], label='loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Binary crossentropy')
+    plt.legend()
+    plt.grid()
+    plt.show()
 
+
+def plot_accuracy(history):
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+def calculate_global_error(y_test, y_pred_bin):
+    counter = 0
+    for i in range(len(y_test)):
+        if y_test[i] != y_pred_bin[i]:
+            counter += 1
+
+    result = round(counter / len(y_test), 8)
+    return result
+
+
+def calculate_individual_error(y_test, y_pred_bin, types_list):
+    individual_error_list = [0] * len(types_list)
+    individual_correct_list = [0] * len(types_list)
+    num_of_individuals = [0] * len(types_list)
+    correct = 0
+
+    for i in range(len(y_test)):
+        for j in range(len(types_list)):
+            if y_test[i] != y_pred_bin[i] and y_test[i] == types_list[j]:
+                individual_error_list[j] += 1
+            elif y_test[i] == y_pred_bin[i] and y_test[i] == types_list[j]:
+                individual_correct_list[j] += 1
+
+    for i in range(len(y_test)):
+        for j in range(len(types_list)):
+            if y_test[i] == types_list[j]:
+                num_of_individuals[j] += 1
+
+    for i in range(len(individual_correct_list)):
+        correct += individual_correct_list[i]
+
+    # print(types_list, "jakie rodzaje")
+    # print(num_of_individuals, "ile elementow kazdego rodzaju")
+    # print(individual_error_list, "ile bledow kazdego rodzaju")
+
+    for i in range(len(individual_error_list)):
+        individual_error_list[i] = individual_error_list[i] / num_of_individuals[i]
+
+    # print(individual_error_list, "procent bledow kazdego rodzaju")
+
+    return individual_error_list, individual_correct_list, correct
+
+
+def test_logs(global_error, indiv_error, report, conf_matrix, filename):
+    with open(filename, 'w') as plik:
+        plik.write("WIELKOSCI Z CZESCI TESTOWEJ\n\nRaport\n")
+        plik.write(report)
+
+        plik.write("\nMacierz pomylek\n")
+        plik.write(str(conf_matrix))
+
+        plik.write("\n\nBlad dla calego wzorca\n")
+        plik.write(str(global_error))
+
+        plik.write("\n\nLista bledow na poszczegolnych wyjsciach \n")
+        for element in indiv_error:
+            plik.write(f"{element}\n")
+
+
+
+def result_logs(y_test, y_pred, indiv_correct, correct, filename):
+    with open(filename, 'w') as plik:
+        plik.write("POROWNANIE WYNIKOW\nLiczba poprawnie sklasyfikowanych elementow:\n")
+        plik.write(str(correct))
+        plik.write("\nZ podzialem na klasy:\n")
+        for element in indiv_correct:
+            plik.write(f"{element}\n")
+        plik.write("Wyniki testowe - Wyniki przewidywane\n")
+        for test, pred in zip(y_test, y_pred):
+            plik.write(f"{test} - {pred}\n")
 
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import tf
+
 from functions import *
 
-cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
-df = pd.read_csv('magic04.data', names=cols)
+# - - - DATA PREPARATION - - -
 
-df["class"] = (df["class"] == "g").astype(int)
+df = pd.read_csv('magic04.data')
+
+mapping = {'g': 0, 'h': 1}
+df = df.replace(mapping).infer_objects(copy=False)
 
 train, valid, test = np.split(df.sample(frac=1), [int(0.6 * len(df)), int(0.8 * len(df))])
 
@@ -11,11 +15,62 @@ train, x_train, y_train = oversample_set(train, True)
 valid, x_valid, y_valid = oversample_set(valid)
 test, x_test, y_test = oversample_set(test)
 
+# - - - SVM - - -
+
 svm_model = SVC()
 svm_model = svm_model.fit(x_train, y_train)
 
-y_pred = svm_model.predict(x_test)
-
-print(classification_report(y_test, y_pred))
+y_pred1 = svm_model.predict(x_test)
 
 
+print(classification_report(y_test, y_pred1))
+
+# - - - NEURAL NETWORK - - -
+
+nn_model = tf.keras.Sequential([
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(32, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+
+nn_model.compile(optimizer=tf.keras.optimizers.Adam(0.001), loss='binary_crossentropy', metrics=['accuracy'])
+
+history = nn_model.fit(x_train, y_train, epochs=100, batch_size=32, validation_split=0.2, verbose=0)
+
+y_pred2 = nn_model.predict(x_test)
+
+# print(classification_report(y_test, y_pred2))
+
+plot_accuracy(history)
+plot_loss(history)
+
+types_list = []
+for i in y_test:
+    if i not in types_list:
+        types_list.append(i)
+
+y_pred_bin = []
+for i in range(len(y_pred2)):
+    if y_pred2[i] >= 0.5:
+        y_pred_bin.append(1)
+    else:
+        y_pred_bin.append(0)
+
+report = classification_report(y_test, y_pred1)
+conf_matrix = confusion_matrix(y_test, y_pred1)
+
+global_error = calculate_global_error(y_test, y_pred1)
+individual_error_list, individual_correct_list, correct = calculate_individual_error(y_test, y_pred1, types_list)
+
+
+file1 = "testing_logs.txt"
+file2 = "testing_results.txt"
+test_logs(global_error, individual_error_list, report, conf_matrix, file1)
+result_logs(y_test, y_pred1, individual_correct_list, correct, file2)
+
+global_error1 = calculate_global_error(y_test, y_pred_bin)
+individual_error_list1, individual_correct_list1, correct1 = calculate_individual_error(y_test, y_pred_bin, types_list)
+file3 = "testing_logs1.txt"
+file4 = "testing_results1.txt"
+test_logs(global_error1, individual_error_list1, report, conf_matrix, file3)
+result_logs(y_test, y_pred_bin, individual_correct_list1, correct1, file4)
